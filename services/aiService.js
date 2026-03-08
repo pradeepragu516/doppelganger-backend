add221054e3d9b00249e9f601ba5cb3b77ccd1ad
pradeepragu516 <pradeep.m2023it@sece.ac.in>
2026-03-08 20:02:57 +0530

    fifth

--- services/aiService.js
+++ services/aiService.js
@@ -1,13 +1,22 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const ML_SERVICE_URL = process.env.ML_SERVICE_URL || 'http://localhost:8000/predict';
+const ML_SERVICE_URL = process.env.ML_SERVICE_URL;
 
 exports.detectAnomaly = async (sensorData) => {
   try {
-    const response = await axios.post(ML_SERVICE_URL, sensorData, { timeout: 5000 });
+
+    const response = await axios.post(
+      `${ML_SERVICE_URL}/predict`,
+      sensorData,
+      { timeout: 5000 }
+    );
+
     return response.data;
+
   } catch (err) {
-    console.error('aiService.detectAnomaly error:', err.message || err);
+
+    console.error("aiService.detectAnomaly error:", err.message);
     throw err;
+
   }
 };
